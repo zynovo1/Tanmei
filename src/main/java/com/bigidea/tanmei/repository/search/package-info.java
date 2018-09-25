12246f0276657c0ef19f3324d9992da42c6a2ba5
git <zynovo@hotmail.com>
2018-09-25 15:43:36 +0800

    Revert "no message"
    This reverts commit 39d9f34b12047f17140eb22f3df58bf9c5638b9d.

--- src/main/java/com/bigidea/tanmei/repository/search/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring Data Elasticsearch repositories.
- */
-package com.bigidea.tanmei.repository.search;
--- src/main/java/com/bigidea/tanmei/service/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Service layer beans.
- */
-package com.bigidea.tanmei.service;
